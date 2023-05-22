4a38ca2458c29ca20187d04fcc689ab07e7ee2ef
Jowrney Kim <jowrney@jowrney.com>
2023-05-22 11:39:30 +0900

    - 페이지 컨텐츠 추가

---
+++ src/pages/layouts/_customers.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+const Customers = () =>{
+    return(
+        <Div>
+            Customers
+        </Div>
+    );
+}
+
+export default Customers;
+
+
+const Div = styled.div`
+   
+`;
